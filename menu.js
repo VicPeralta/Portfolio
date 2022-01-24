d3ffdb7821e455191295bbd439cc4634bcc01664
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-01-24 09:55:20 -0600

    Add menu options, formating and basic functionality
    modified:   index.html
    new file:   menu.css
    new file:   menu.js

---
+++ menu.js
@@ -0,0 +1,32 @@
+
+function makeScrollable() {
+  let body = document.querySelector('body');
+  body.classList.remove('makeNotScrollable');
+  body.classList.add('makeScrollable');
+}
+
+function makeNotScrollable() {
+  let body = document.querySelector('body');
+  body.classList.remove('makeScrollable');
+  body.classList.add('makeNotScrollable');
+}
+
+function closeMenu() {
+  let menu = document.querySelector('.menu-options');
+  menu.classList.remove('visible');
+  menu.classList.add('invisible');
+  makeScrollable();
+}
+
+let optionsBtn = document.querySelector(".hamburger-button");
+optionsBtn.addEventListener('click', e => {
+  let menu = document.querySelector('.menu-options');
+  menu.classList.remove('invisible');
+  menu.classList.add('visible');
+  makeNotScrollable();
+});
+
+let closeOption = document.querySelector("#close-btn");
+closeOption.addEventListener('click', e => {
+  closeMenu();
+});
